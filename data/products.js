ae6a9a799557d5b0e51cd44e14d0117302636637
Oyinuah Simeni <oyinuahsimeni@gmail.com>
2025-10-23 21:52:28 +0100

    Add data/products.js with initial product inventory and product required line in the server.js file

---
+++ data/products.js
@@ -0,0 +1,14 @@
+const products = {
+    "Samsung S22 Ultra": {
+        id: 1,
+        price: 600,
+        Instock: 10
+    },
+    "Apple iPhone 14 Pro": {
+        id: 2,
+        price: 800,
+        Instock: 3
+    }
+};
+
+module.exports =products;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').congig(); //Calling the environment variables
 const express = require('express'); //Calling the express fnction from node_modules
 const app = express();
 const PORT = process.env.PORT;
+const products = require('./data/products');
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
