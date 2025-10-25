355b8916154d89c7ac9ea20ade3df76b8c98cfe7
Oyinuah Simeni <oyinuahsimeni@gmail.com>
2025-10-25 12:11:10 +0100

    Simeni: changed variable declaration to LET, removed the key-identifier from the product list

--- data/products.js
+++ data/products.js
@@ -1,20 +1,20 @@
 // data/products.js
 
-const products = {
+let products = [
   // Samsung
-  samsung_s22: {
+  {
     id: 1,
     name: "Samsung S22 Ultra",
     price: 600,
     InStock: 10
   },
-  samsung_a54: {
+  {
     id: 2,
     name: "Samsung Galaxy A54",
     price: 400,
     InStock: 15
   },
-  samsung_zflip5: {
+  {
     id: 3,
     name: "Samsung Z Flip 5",
     price: 900,
@@ -22,19 +22,19 @@ const products = {
   },
 
   // iPhone
-  iphone_14pro: {
+  {
     id: 4,
     name: "iPhone 14 Pro",
     price: 1200,
     InStock: 8
   },
-  iphone_13: {
+  {
     id: 5,
     name: "iPhone 13",
     price: 950,
     InStock: 10
   },
-  iphone_se: {
+  {
     id: 6,
     name: "iPhone SE (2022)",
     price: 500,
@@ -42,19 +42,19 @@ const products = {
   },
 
   // Pixel
-  pixel_7pro: {
+  {
     id: 7,
     name: "Google Pixel 7 Pro",
     price: 850,
     InStock: 9
   },
-  pixel_6a: {
+  {
     id: 8,
     name: "Google Pixel 6a",
     price: 450,
     InStock: 14
   },
-  pixel_fold: {
+  {
     id: 9,
     name: "Google Pixel Fold",
     price: 1300,
@@ -62,19 +62,19 @@ const products = {
   },
 
   // Huawei
-  huawei_p60pro: {
+  {
     id: 10,
     name: "Huawei P60 Pro",
     price: 700,
     InStock: 7
   },
-  huawei_mate50: {
+  {
     id: 11,
     name: "Huawei Mate 50",
     price: 800,
     InStock: 6
   },
-  huawei_nova11: {
+  {
     id: 12,
     name: "Huawei Nova 11",
     price: 550,
@@ -82,24 +82,24 @@ const products = {
   },
 
   // Motorola
-  moto_edge40: {
+  {
     id: 13,
     name: "Motorola Edge 40",
     price: 600,
     InStock: 8
   },
-  moto_g73: {
+  {
     id: 14,
     name: "Motorola G73",
     price: 350,
     InStock: 15
   },
-  moto_razr40: {
+  {
     id: 15,
     name: "Motorola Razr 40",
     price: 900,
     InStock: 5
   }
-};
+];
 
 module.exports = products;
