2f4ba1d59707ff07af34e5528f59e1f77e75ec2d
Oyinuah Simeni <oyinuahsimeni@gmail.com>
2025-10-25 07:04:04 +0100

    Simeni: added Root homepage ofthe API

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').congig(); //Calling the environment variables
+require('dotenv').config(); //Calling the environment variables
 
 const express = require('express'); //Calling the express fnction from node_modules
 const app = express();
@@ -10,3 +10,9 @@ app.listen(PORT, () => {
 });
 
 app.use(express.json()); //In-built express middleware for json parsing
+
+// Root route (homepage) of the API
+app.get('/', (req, res) => {
+  // Send a simple text response to confirm the API is running
+  res.send('Product Inventory API is running');
+});
