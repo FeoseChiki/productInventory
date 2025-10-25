7ad9cf6668f9e0144818d49ab72155617c2475dd
Oyinuah Simeni <oyinuahsimeni@gmail.com>
2025-10-25 07:22:05 +0100

    Simeni: added Route to get all products

--- server.js
+++ server.js
@@ -16,3 +16,20 @@ app.get('/', (req, res) => {
   // Send a simple text response to confirm the API is running
   res.send('Product Inventory API is running');
 });
+
+// Route to get all products
+app.get('/products', (req, res) => {
+  // Check if the products array is empty
+  // If there are no products, return a 404 status with a message
+  if (products.length === 0) {
+    return res.status(404).json({
+      message: 'No products found',
+      data: []
+    });
+  }
+  // If products exist, send them back with a 200 (OK) status
+  res.status(200).json({
+    message: 'Products retrieved successfully',
+    data: products
+  });
+ });
